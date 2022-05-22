e2b549e865b2fb34afdd5527b9287d8c91c227bd
Boris Krasko <boris.krasko@live.com>
2022-05-22 17:13:44 +0200

    feat: re-build server with Express

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const http = require('http');
+// const http = require('http');
+
+// const PORT = process.env.PORT || 5000;
+
+// const server = http.createServer((req, res ) => {
+//   res.writeHead(200, {
+//     'Content-type': 'text/HTML;'
+//   })
+//   res.end(`{${new Date()}}`)
+// })
+
+// server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+
+
+const express = require('express');
+const app = express();
 const cors = require('cors');
+const { set } = require('express/lib/application');
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res ) => {
+app.get('/', cors(), (req, res ) => {
   res.writeHead(200, {
     'Content-type': 'text/HTML;'
   })
-  res.end(`{${new Date()}}`)
+  res.end(JSON.stringify(new Date()));
 })
 
-server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+app.use(cors({
+  origin: '*'
+}));
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
