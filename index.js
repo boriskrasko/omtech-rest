6e3fe4e375b412996c68eede58538f91d5c103f1
Boris Krasko <boris.krasko@live.com>
2022-05-22 14:46:35 +0200

    Co-authored-by: Ivan Tur <buiiz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer( (req, res ) => {
+const server = http.createServer((req, res ) => {
   res.writeHead(200, {
     'Content-type': 'text/HTML;'
   })
-  res.end(`<h1>${new Date()}</h1>`)
+  res.end(`{${new Date()}}`)
 })
 
 server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
