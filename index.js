9f642e7e904fbf1e62dacb7e6244a39fabcb5cf7
Boris Krasko <boris.krasko@live.com>
2022-05-22 19:59:39 +0200

    refactor: remove commented code

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-// const http = require('http');
-
-// const PORT = process.env.PORT || 5000;
-
-// const server = http.createServer((req, res ) => {
-//   res.writeHead(200, {
-//     'Content-type': 'text/HTML;'
-//   })
-//   res.end(`{${new Date()}}`)
-// })
-
-// server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
-
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { set } = require('express/lib/application');
 
 const PORT = process.env.PORT || 5000;
 
